b655e1352845f1f41dfc88bde6d518fe2688d2e2
Tobias Skoglund <inbox.tskoglund@gmail.com>
2014-11-05 17:18:46 +0100

    Added xorg and updated bspwm

---
+++ install.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+PKG_MGR_CMD="sudo pacman -S "
+
+function install_bspwm() {
+	echo "* Installing bspwm"
+	yaourt -S bspwm 
+}
+
+function configure_bspwm() {
+	echo "* Installing bspwm"
+	mkdir $HOME/.config/bspwm
+	cp wm/bspwm/bspwmrc $HOME/.config/bspwm/
+	mkdir $HOME/.config/sxhkd/
+	cp wm/bspwm/sxhkd $HOME/.config/sxhkd/
+	chmod +x $HOME/.config/sxhkd/sxhkdrm
+}
+
+function configure_xorg() {
+	echo "* Configuring Xorg"
+	cp xorg/xinitrc $HOME/.xinitrc
+	cp xorg/Xdefaults $HOME/.Xdefaults
+}
+
+configure_xorg
+install_bspwm
+configure_bspwm
